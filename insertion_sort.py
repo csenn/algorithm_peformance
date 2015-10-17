4e8f1127a628d689e9d2852bfa386e9f4d19d0ed
Craig Sennabaum <csennabaum@gmail.com>
2015-10-17 09:12:08 -0700

    Clean up with PyCharm

--- insertion_sort.py
+++ insertion_sort.py
@@ -1,22 +1,20 @@
-
 # ( n^2 - n ) / 2
 # n^2
 def insertion_sort(arr):
+    def shift_right(right_index):
 
-	def shift_right(right_index):
+        start_val = arr[right_index]
 
-		start_val = arr[right_index]
+        while (right_index >= 1):
+            if arr[right_index] >= arr[right_index - 1]:
+                break
+            temp = arr[right_index]
+            arr[right_index] = arr[right_index - 1]
+            arr[right_index - 1] = temp
+            right_index -= 1
 
-		while(right_index >= 1):
-			if arr[right_index] >= arr[right_index - 1]:
-				break
-			temp = arr[right_index]
-			arr[right_index] = arr[right_index - 1]
-			arr[right_index - 1] = temp
-			right_index -= 1
+    arr_len = len(arr)
+    for idx in range(1, arr_len):
+        shift_right(idx)
 
-	arr_len = len(arr)
-	for idx in range(1, arr_len):
-		shift_right(idx)
-
-	return arr
+    return arr
--- palindrome.py
+++ palindrome.py
@@ -1,17 +1,19 @@
-
 # 'ROTOR'
 # 'MOTOR'
 
+
 def is_palindrome(word):
-	flipped_word = ''
-	# Clean Reverse Syntax
-	for i in reversed(xrange(len(word))):
-		flipped_word += word[i]
-	return flipped_word == word
+    flipped_word = ''
+    # Clean Reverse Syntax
+    for i in reversed(xrange(len(word))):
+        flipped_word += word[i]
+    return flipped_word == word
+
 
 def is_palindrome_recursive(word):
-	def flip(index):
-		if index == 0:
-			return word[0]
-		return word[index] + flip(index - 1)
-	return word == flip(len(word) - 1)
+    def flip(index):
+        if index == 0:
+            return word[0]
+        return word[index] + flip(index - 1)
+
+    return word == flip(len(word) - 1)
